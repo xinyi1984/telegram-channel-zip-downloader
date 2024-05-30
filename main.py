9a40afd09826c85845d7fccb78b3e0eb632f0728
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2024-05-30 10:26:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from telethon.sync import TelegramClient
 from telethon.tl.types import MessageMediaDocument
+import requests
 
 # ************************************************************
 #                       配置信息
@@ -12,8 +13,17 @@ from telethon.tl.types import MessageMediaDocument
 # 填写资料点击创建 会生成 api_id api_hash
 # 网页点击create application按钮创建时 经常会提示失败
 # 没有明确的解决方法 在不同的时段多试几次 有几率就成功了 有时候好几天都不行
-api_id = 26183977
+api_id = '26183977'
 api_hash = 'f20004c868bd9e626e6bf815343518ee'
+api key =''
+def delete telegram app(api id, api hash, api key):
+ur1 = f"https://my.telegram.org/auth/revokeApp?api_id={api_id}&api_hash={api_hash}&api_key={api_key}"
+response =reguests.get(ur1)
+if response.status code == 200:
+print("Telegram应用程序已成功删除!")
+else:
+print(“无法删除Telegram应用程序。")
+delete telegram app(api id, api hash, api key)
 
 # 定义会话名称，可以随便指定，确保唯一即可
 #
